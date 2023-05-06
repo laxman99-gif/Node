91ded90762509b13796817a53422cb53b67c9426
laxman99 <parajulil385@gmail.com>
2023-05-06 11:56:53 +0545

    Post Created

--- routes.js
+++ routes.js
@@ -1,30 +1,17 @@
 const express = require("express");
 const router = express.Router();
-router.get("/hi",function(req,res ){
-    console.log(req.query)
-    if(req.query.name != undefined){
-        res.send("Hello "+ req.query.name)
+const createNewPost = require("./function");
 
-    }else{
-        res.send("hello")
-    }
-    
-    //Getting jskon data from request body 
-    
-    
-})
-router.post("/bye",function(req,res){
-    console.log(req.body.height)
-    res.send("Hey myan")
-})
-router.get("/",function(req,res ){
-    res.send("BYe BYe")
-    console.log("New request found")
-    
-})
-router.post("/",function(req,res ){
-    res.send("NEw post method")
-    console.log("New request found")
-    
-})
+router.post("/", function (req, res) {
+  // { postContent: 'Post from API', postCreatedBy: 'THunder Client' }
+
+  createNewPost(req.body.postContent, req.body.postCreatedBy)
+    .then(function (post) {
+      res.send("Post Created successfully");
+    })
+    .catch(function (error) {
+      res.send("Error Creating Post");
+    });
+});
+
 module.exports = router;
